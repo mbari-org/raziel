a54596aec8232375b1dc769dc6429bc2c137bd73
Brian Schlining <bschlining@gmail.com>
2021-12-30 15:20:24 -0800

    Using docker buildx

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ ARCH=$(uname -m)
 if [[ $ARCH == 'arm64' ]]; then
     sbt 'Docker / stage'
     cd $SCRIPT_DIR/target/docker
+    VCS_REF=`git tag | sort -V | tail -1`
     # https://betterprogramming.pub/how-to-actually-deploy-docker-images-built-on-a-m1-macs-with-apple-silicon-a35e39318e97
     docker buildx build \
       --platform linux/amd64,linux/arm64 \
+      -t mbari/raziel:${VCS_REF} \
       -t mbari/raziel:latest \
-      --load \
       --push .
 else
     sbt 'Docker / publish'
