0670ba4416368cf715ddca194829260a2b726ea2
Brian Schlining <bschlining@gmail.com>
2021-12-30 16:28:51 -0800

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo $SCRIPT_DIR
 
 ARCH=$(uname -m)
 if [[ $ARCH == 'arm64' ]]; then
     sbt 'Docker / stage'
-    cd $SCRIPT_DIR/target/docker
+    cd $SCRIPT_DIR/target/docker/stage
     VCS_REF=`git tag | sort -V | tail -1`
     # https://betterprogramming.pub/how-to-actually-deploy-docker-images-built-on-a-m1-macs-with-apple-silicon-a35e39318e97
     docker buildx build \
