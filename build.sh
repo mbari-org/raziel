fe5ff0b0cf4e3f3c3c1ba76717a0c6c32c8866cb
Brian Schlining <bschlining@gmail.com>
2022-08-02 10:42:37 -0700

    Fixed tag parsing in build.sh

--- build.sh
+++ build.sh
@@ -7,14 +7,16 @@ ARCH=$(uname -m)
 if [[ $ARCH == 'arm64' ]]; then
     sbt 'Docker / stage'
     cd $SCRIPT_DIR/target/docker/stage
-    VCS_REF=`git tag | sort -V | tail -1`
+    VCS_REF=`git tag | sort -V | grep -E '^\d' | tail -1`
     # https://betterprogramming.pub/how-to-actually-deploy-docker-images-built-on-a-m1-macs-with-apple-silicon-a35e39318e97
     docker buildx build \
       --platform linux/amd64,linux/arm64 \
       -t mbari/raziel:${VCS_REF} \
       -t mbari/raziel:latest \
       --push . \
-    && docker pull mbari/raziel:latest
+    && docker pull mbari/raziel:${VCS_REF}
 else
     sbt 'Docker / publish'
 fi
+
+#       -t mbari/raziel:latest \
