69cc2a2ea871d0392bc9a20d6a8a8a9ba1a9b97d
Brian Schlining <bschlining@gmail.com>
2023-11-14 10:55:46 -0800

    Updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,14 +20,14 @@ object Dependencies {
   lazy val slf4jApi     = "org.slf4j" % "slf4j-api"    % slf4jVersion
   lazy val slf4jJul     = "org.slf4j" % "jul-to-slf4j" % slf4jVersion
 
-  private val tapirVersion  = "1.8.4"
+  private val tapirVersion  = "1.9.0"
   lazy val tapirStubServer  = "com.softwaremill.sttp.tapir" %% "tapir-sttp-stub-server" % tapirVersion
   lazy val tapirSwagger     = "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion
   lazy val tapirCirce       = "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion
-  lazy val tapirCirceClient = "com.softwaremill.sttp.client3" %% "circe" % "3.9.0"
+  lazy val tapirCirceClient = "com.softwaremill.sttp.client3" %% "circe" % "3.9.1"
   lazy val tapirVertx       = "com.softwaremill.sttp.tapir" %% "tapir-vertx-server" % tapirVersion
  
     
   lazy val typesafeConfig = "com.typesafe" % "config" % "1.4.3"
-  lazy val zio            = "dev.zio"     %% "zio"    % "2.0.18"
+  lazy val zio            = "dev.zio"     %% "zio"    % "2.0.19"
 }
